74504c55d787d20dfbd2a043a4246ca3bbbda826
Umer Ali <umer30962@gmailcom>
2025-07-02 16:30:06 +0500

    Add initial JavaScript code with commented examples for learning purposes

---
+++ script.js
@@ -0,0 +1,68 @@
+// console.log("Hello, World!");
+// let name = "Umer";
+// const age = 21;
+
+
+// name = "Ali"
+// // age  = 22;                     gives error
+// console.log(name, age);
+
+// let passed = !true;
+// console.log(passed);
+
+
+// let marks = 50
+
+// if (marks >= 80)
+// {
+//     console.log("A+");
+
+// }
+// else if (marks >= 70)
+// {
+//     console.log("A");
+// } else if (marks >= 60)
+// {
+//     console.log("B");
+// }else if (marks >= 50)
+// {
+//     console.log("C");
+// }
+
+
+// let fruits = null;
+// fruits = "apple";
+// switch (fruits)
+// {
+// case "apple":
+//     console.log("Apple is red")
+//     break;
+// case "banana":
+//     console.log("Banana is yellow")
+//     break;
+// case "grape":
+//     console.log("Grape is purple")
+//     break;
+// default:
+//     console.log("Unknown fruit")
+// }
+
+
+// for(let i =0; i<5;i++){
+
+//     console.log("*");
+// }
+
+
+// let i =0;
+
+// while(i<5){
+//     console.log("*");
+//     i++
+// }
+
+// let colors = ["red", "blue", "green"];
+
+// colors.forEach((color ,index) => {
+//   console.log(index , color);
+// });
